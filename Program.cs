340a611abc54bba1e475b5318778d7b69444d15f
erey21 <arinerey@yandex.ru>
2021-11-04 22:01:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework___6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание № 6");
+            Console.WriteLine("Введите номер операции: \n1. Сложение \n2. Вычитание \n3. Умножение");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input) {
+                case 1:
+                    Console.WriteLine("Сложение");
+                    break;
+                case 2:
+                    Console.WriteLine("Вычитание");
+                    break;
+                case 3:
+                    Console.WriteLine("Умножение");
+                    break;
+                default:
+                    Console.WriteLine("Операция не определена");
+                    break;
+            }
+        }
+    }
+}
